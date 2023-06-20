104ec60701ce4b9e8a4be2c4b9cf5a1d70d3e15d
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-19 23:14:37 -0700

    initial commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { getAllPostsIDs, getPostData } from "../../lib/posts";
+import { parseDate } from "../../lib/date";
+
+import styles from "../../styles/utils.module.css";
+
+export async function getStaticPaths() {
+  const paths = getAllPostsIDs();
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: { postData },
+  };
+}
+
+const Post = ({ postData }) => {
+  const date = parseDate(postData.date);
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {postData.id}
+      <br />
+      <h1 className={styles.headingXl}>{postData.title}</h1>
+      <br />
+      <div className={styles.lightText}>{date}</div>
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+};
+
+export default Post;
